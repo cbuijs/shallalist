36228da1dbf463741432a1b107e5c56527871b03
Chris Buijs <cbuijs@chrisbuijs.com>
2018-05-16 17:13:56 -0400

    Update List 20180516-171356

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /opt/shallalist
+
+rm -f /tmp/shalla.tgz
+wget -4 "http://www.shallalist.de/Downloads/shallalist.tar.gz" -O /tmp/shalla.tgz
+
+git rm -r .
+tar --strip-components=1 -zxvf /tmp/shalla.tgz -C /opt/shallalist
+
+DATE=`date +%Y%m%d-%H%M%S`
+echo "${DATE}" > last.updated
+git add .
+git commit -m "Update List ${DATE}"
+git push origin master
+
+exit 0
+
